d623211d6eb455c5065ea7f2a92aba8f6aeebecf
Stepan Melnichuk <stepan.melnichuk@mail.ru>
2017-08-10 11:49:26 +0300

    Тестовое задание

--- src/ru/testovoezadanie/main/LengthComparator.java
+++
@@ -1,8 +0,0 @@
-package ru.testovoezadanie.main;
-
-public class LengthComparator implements  java.util.Comparator<String> {
-    @Override
-    public int compare(String o1, String o2) {
-        return o1.length()-o2.length();
-    }
-}
